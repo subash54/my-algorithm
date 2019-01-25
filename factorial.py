6e6ce00fc3e9c7c8f7828a8732948927ff7d8932
subash54 <44032117+subash54@users.noreply.github.com>
2019-01-25 06:35:42 +0530

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,19 @@
+# factorial
+a = int(input("enter the num"))
+f = 1
+while (a != 0):
+    f = f * a
+    a -= 1
+print("the factorial of is", f)
+
+
+# recursion factorial
+def fact(n):
+    if n == 1:
+        return 1
+    else:
+        return n * fact(n - 1)
+
+
+b = int(input("enter the num"))
+print(fact(b))
